7f0434e7775b72f04d05e9f4d255a8755518a661
Rauf <kurdrauf74@gmail.com>
2026-02-07 22:52:16 +0300

    feat(tasks): add optimistic delete for tasks.

--- src/features/tasks/useDeleteTask.js
+++ src/features/tasks/useDeleteTask.js
@@ -6,13 +6,28 @@ function useDeleteTask() {
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationFn: (id) => deleteTask(id),
+    onMutate: async (id) => {
+      await queryClient.cancelQueries(["tasks"]);
+
+      const previous = queryClient.getQueryData(["tasks"]);
+
+      queryClient.setQueryData(["tasks"], (old = []) =>
+        old.filter((task) => task.id !== id),
+      );
+      return { previous };
+    },
     onSuccess: () => {
       toast.success("Task deleted");
-      queryClient.invalidateQueries({ queryKey: ["tasks"] });
     },
-    onError: (error) => {
-      toast.error("There was an error while deleting task");
+    onError: (error, vars, context) => {
+      if (context?.previous) {
+        queryClient.setQueryData(["tasks"], context.previous);
+      }
+      toast.error("There was an error while deleting task, changes reverted");
       console.log("Error:", error.message);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["tasks"]);
     },
   });
   return { mutate, isPending };
